f1ff1abd2baa7523887d62d434289cfae364597c
weslley <weslleysouza22@gmail.com>
2021-08-19 01:00:15 -0300

    Display for calculator with js function.

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.querySelector('body').addEventListener('keydown', function(event) {
+	let tecla = event.keyCode;
+	if(tecla == 13) {
+		
+	} 
+});
+function displaynum(n1){
+    Calculator.text1.value =  Calculator.text1.value + n1;
+}
